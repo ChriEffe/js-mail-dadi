8833ae7246d995afdadec368ebc8122a9dd39e30
ChriEffe <christianferrazzano93@gmail.com>
2021-11-24 16:39:53 +0100

    Dadi

--- dadi/js-dadi/script-dadi.js
+++ dadi/js-dadi/script-dadi.js
@@ -1,2 +1,21 @@
 /*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+// Lancio utente
+function randomUser() {
+    let numUser = Math.floor(Math.random() * 6) + 1;
+
+// Lancio computer
+    let numComputer = Math.floor(Math.random() * 6) + 1;
+
+// Condizioni
+    if (numUser > numComputer) {
+        document.writeln('Vincitore Utente con ' + numUser + ' a ' + numComputer);
+    } else if (numUser < numComputer) {
+        document.writeln('Vincitore Computer con ' + numComputer + ' a ' + numUser);
+    } else {
+        document.writeln('Pareggio');
+    }
+}
+
+
